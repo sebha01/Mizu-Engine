c37c631070c4da3445a1c32008542d9f9a660ec0
sebha01 <30077548@students.southwales.ac.uk>
2026-07-09 12:00:13 +0100

    Moved shader code into vert and frag file, created new shader header file

--- Mizu Engine/Renderer/Renderer.cpp
+++ Mizu Engine/Renderer/Renderer.cpp
@@ -1,13 +1,5 @@
 #include "Renderer.h"
 
-//Shaders
-//Vertex Shader Source Code
-const char* vertexShaderSource = "#version 330 core\n"
-	"layout (location = 0) in vec3 aPos;\n"
-	"void main()\n"
-	"{\n"
-	"   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
-	"}\0";
 //Fragment shader source code
 const char* fragmentShaderSource = "#version 330 core\n"
 	"out vec4 FragColor;\n"
